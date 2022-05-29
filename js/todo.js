b17836f6a1384a4d6963f79e0cdf056c0b37aca3
jinju-choi <cjj4643@gmail.com>
2022-05-29 16:05:13 +0900

    투두리스트 로컬스트리지 저장하는 기능 구현

--- js/todo.js
+++ js/todo.js
@@ -4,13 +4,16 @@ const toDoList = document.getElementById("todo-list");
 
 // localStorage 에 저장하기 위해 li가 생성 될때마다 배열에 삽입
 // localStorage 에는 배열 저장 안됨. 텍스트만 가능
-const toDos = [];
 
+const TODOS_KEY = "todos";
 
+let toDos = [];
+// toDos에 받아온 배열을 문자열로 변환해 localStrage에 저장
 function saveToDos() {
-  localStorage.setItem("todos", toDos)
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
+// 버튼을 클릭하면 클릭한 요소의 부모요소 li를 제거
 // parentElement 부모요소 
 function deleteTodo(event){
   // 클릭한 요소의 부모요소
@@ -18,6 +21,8 @@ function deleteTodo(event){
   li.remove();
 }
 
+
+// 투두리스트를 화면에 보여주는 코드
 function paintToDo(newTodo) {
   // li요소 생성하는 변수
   const li = document.createElement("li");
@@ -33,18 +38,38 @@ function paintToDo(newTodo) {
   li.appendChild(span);
   li.appendChild(button);
 
-
   toDoList.appendChild(li);
 }
 
+// input에 값을 입력했을 때 실행되는 함수
 function handleToDoSubmit(event) {
   event.preventDefault();
-
-  // 인풋 갑을 먼저 변수에 복사
   const newTodo = toDoInput.value;
-  // 엔터를 누르면 빈값을 넣기
   toDoInput.value = "";
+
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+
+// localStorage에 저장되어있는 "toDos" 의 값을 가져옴
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+// localStorage에 저장되어있는 "toDos" 의 값이 있으면 실행
+// localStorage에 저장되어있는 "toDos" 의 값을 forEach로 parsedToDos배열에 있는 수 만큼 paintToDo함수를 실행 
+if(savedToDos) {
+  // JSON.sringify() 로 string으로 되어있는 값을 array로 다시 변환(forEach문을 쓰기 위함)
+  const parsedToDos = JSON.parse(savedToDos);
+  parsedToDos.forEach(paintToDo);
+  toDos = parsedToDos;
+}
+
+
+// 새로고침하고 투두리스트를 작성하면 원래 있던 배열에 값이 덮어 씌워짐
+// 원인은 const toDos = []; 에 배열이 비어있는 상태로 실행되기 때문
+// 오직 새로 입력한 newTodo의 값만 toDos배열에 들어가기 때문에 이전에 있는 localStorage는 덮어짐
+// 해결 방법은 const toDos = []; 를 let toDos = [];로 변경하고
+//  localStorage에 저장되어있는 "toDos" 의 값이 있으면  toDos = parsedToDos (로컬에 들어있는 값)를 넣어주면 됨 
